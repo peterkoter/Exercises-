06246677b372faf207bfa9e14a8aec8481b05c7b
peterkoter <peterkoter@users.noreply.github.com>
2016-01-17 20:31:50 +0200

    Update checkPermutations.groovy
    made method and changed little bit the code

--- checkPermutations.groovy
+++ checkPermutations.groovy
@@ -1,12 +1,14 @@
 /*
-It's a short code which tell's if two random strings, even with
-integers inside, are permutations of each other or not
+It returns if two random strings are permutations of each other or not
  */
-def string1 = "acBE2d".toUpperCase().toList().sort().join('')
-def string2 = "e2dCAb".toUpperCase().toList().sort().join('')
-if (string1==string2) {
-    println "These are permutations of each other"
-} else {
-    println "These are not permutations of each other"
+def checkPermutations(String txt1, String txt2) {
+    def string1 = txt1.toUpperCase().toList().sort()
+    def string2 = txt2.toUpperCase().toList().sort()
+    if (string1 == string2) {
+        println 'These are permutations of each other'
+    } else {
+        println 'These are not permutations of each other'
+    }
 }
-//TODO make method and optimize the code
+
+checkPermutations("acBE234#d", "e2dC43Ab#1")
